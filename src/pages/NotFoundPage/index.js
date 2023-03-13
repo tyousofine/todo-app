325fe3b020f3313b94d23053fabcf2c4ac50050b
Tina y <17134939+tyousofine@users.noreply.github.com>
2023-03-13 16:07:16 -0400

    pagenotfound adjustments

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TasksPage from '../TasksPage'
+import './styles.scss'
 
 
 export default function NotFoundPage() {
+    const book = require('./../../images/book.jpg');
+
 
     return (
-        <>
-            <div>Page not found</div>
+        <div className='page-not-found'>
+            <img src={book} alt="image of a book with missing page" />
 
-            <div>click <Link to='/' element={<TasksPage />}>here </Link>to go back to home page</div>
-        </>
+            <div className='oops'>Page not found!</div>
+            <div className='page-not-fount-redirect'>click          <Link to='/' element={<TasksPage />}>here </Link> to go back to home page
+            </div>
+
+        </div>
     )
 }
